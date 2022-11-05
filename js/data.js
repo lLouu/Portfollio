f30443824cd102e58744f9afc7258e3568c3d50b
lLouu <lLouu@users.noreply.github.com>
2022-11-05 21:50:29 +0100

    V0.1.3
    ## Changelog
    - Skills part made

--- js/data.js
+++ js/data.js
@@ -109,4 +109,46 @@ const content = {
             ]
         ]
     },
+    comp: [
+        {
+            template: [false, true, false, true, true, true, false, true, false],
+            src: ['img/emp.png', 'img/min.png', 'img/curieux.png', 'img/clame.png', 'img/per.png'],
+            desc: {
+                fr: ['Empathique', 'Minutieux', 'Curieux', 'Calme', 'Persévérant'],
+                en: ['Empathetic', 'Perfectionist', 'Curious', 'Calm', 'Persevering']
+            }
+        },
+        {
+            template: [true, true, true, true, false, true, true, true, true],
+            src: ['img/c.png', 'img/py.png', 'img/cs.png', 'img/lua.png', 'img/solidity.png', 'img/wamp.png', 'img/js.png', 'img/bash.png'],
+            desc: {
+                fr: ['C | C++', 'Python', 'C# | .NET', 'Lua', 'Solidity', 'WAMP', 'JavaScript', 'Bash'],
+                en: ['C | C++', 'Python', 'C# | .NET', 'Lua', 'Solidity', 'WAMP', 'JavaScript', 'Bash']
+            }
+        },
+        {
+            template: [true, false, true, false, false, false, true, false, true],
+            src: ['img/windows.png', 'img/linux.png', 'img/vsc.png', 'img/vs.png'],
+            desc: {
+                fr: ['Windows', 'Linux', 'VS Code', 'Visual Studio'],
+                en: ['Windows', 'Linux', 'VS Code', 'Visual Studio']
+            }
+        },
+        {
+            template: [true, false, true, true, true, true, true, false, true],
+            src: ['img/github.png', 'img/azure.png', 'img/figma.png', 'img/visualparadigm.png', 'img/googlesuite.png', 'img/vmware.png', 'img/docker.png'],
+            desc: {
+                fr: ['Github', 'Azure', 'Figma', 'Visual Paradigm', 'Google Suite', 'VMWare', 'Docker'],
+                en: ['Github', 'Azure', 'Figma', 'Visual Paradigm', 'Google Suite', 'VMWare', 'Docker']
+            }
+        },
+        {
+            template: [false, true, false, true, false, true, false, false, false],
+            src: ['img/discord.png', 'img/slack.png', 'img/teams.png'],
+            desc: {
+                fr: ['Discord', 'Slack', 'Microsoft Teams'],
+                en: ['Discord', 'Slack', 'Microsoft Teams']
+            }
+        }
+    ]
 }
--- js/script.js
+++ js/script.js
@@ -112,9 +112,42 @@ const update_parcoure = async (n) => {
     document.getElementById('details_title').innerHTML = content['parcoure'][lang][parcoure_sel][0];
     document.getElementById('details').innerHTML = content['parcoure'][lang][parcoure_sel][1];
 }
+const update_competence = (go_right) => {
+    let d = document.getElementById('c_content');
+    d.style.display = "none";
+    setTimeout(() => {d.style.display = "grid";}, comp_animation)
+    document.getElementById('c_nav').children[comp_sel < 2 ? 0 : 1].children[comp_sel < 2 ? comp_sel : comp_sel - 2].classList.remove('sel');
+    comp_sel = go_right ? (comp_sel + 1) % 5 : (comp_sel + 4) % 5;
+    document.getElementById('c_nav').children[comp_sel < 2 ? 0 : 1].children[comp_sel < 2 ? comp_sel : comp_sel - 2].classList.add('sel');
+    d.innerHTML = '';
+    let count = 0;
+    for(let i = 0; i < 9; i++){
+        if(content['comp'][comp_sel]['template'][i]){
+            let c = document.createElement('div');
+            c.style.gridRow = (Math.floor(i/3) + 1) * 2;
+            c.style.gridColumn = (i%3 + 1) * 2;
+            c.style.display = 'flex';
+            c.style.flexDirection = 'column';
+            c.style.alignItems = 'center';
+            c.style.justifyContent = 'space-evenly';
+            let im = document.createElement('img');
+            im.src = content['comp'][comp_sel]['src'][count];
+            c.appendChild(im);
+            let t = document.createElement('div');
+            t.innerHTML = content['comp'][comp_sel]['desc'][is_fr ? 'fr' : 'en'][count];
+            t.style.textAlign = 'center'
+            count = count + 1;
+            c.appendChild(t);
+            d.appendChild(c);
+        }
+    }
+}
+let competence_auto_scroll = setInterval(() => {update_competence(true)}, comp_auto_scroll);
+
 
 window.addEventListener('DOMContentLoaded', (event) => {
     update_lang();
+    update_competence(true);
     document.querySelectorAll('.button').forEach(ele => {ele.onclick = (e) => {
         document.getElementById('main').scrollBy({
             top : document.getElementById(e.target.dataset.goto).getBoundingClientRect().y - 50,
@@ -128,6 +161,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
         flags[is_fr].classList.add('grayed');
         update_lang();
     }
+    document.getElementById('left_arrow').onclick = (e) => {clearInterval(competence_auto_scroll); update_competence(false); competence_auto_scroll = setInterval(() => {update_competence(true)}, comp_auto_scroll);};
+    document.getElementById('right_arrow').onclick = (e) => {clearInterval(competence_auto_scroll); update_competence(true); competence_auto_scroll = setInterval(() => {update_competence(true)}, comp_auto_scroll);};
     
     document.getElementById('main').addEventListener('scroll', (e) => {
         update_scrollbar(scroll_array.indexOf(document.elementFromPoint(0, document.documentElement.scrollTop + document.documentElement.scrollHeight / 2).id));
